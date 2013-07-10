22cf1d32c2d4a10a50c12ce66fb9b58457c0fc0d
Ubuntu <ubuntu@ip-10-252-174-92.us-west-2.compute.internal>
2013-07-10 19:29:19 +0000

    upgraded grader to take urls and increased the number of checks in checks.json

--- grader.js
+++ grader.js
@@ -20,11 +20,13 @@ References:
  - https://developer.mozilla.org/en-US/docs/JSON#JSON_firefox_2
 */
 
+var rest = require('restler');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://fierce-reaches-1073.herokuapp.com"; 
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -34,6 +36,15 @@ var assertFileExists = function(infile){
     }
     return instr;
 };
+
+var assertURLExists = function(url) {
+ rest.get(url).on('complete', function(result){
+	if (result instanceof Error){
+	    console.log('Error: ' + result.message);
+	    process.exit(1);
+	}
+    return result.toString;
+ })};
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -64,6 +75,7 @@ if(require.main==module){
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists),HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to a web page',clone(assertURLExists),URL_DEFAULT) 
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
